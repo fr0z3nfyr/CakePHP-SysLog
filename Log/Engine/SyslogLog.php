3147f8061972cce757e54ee747a49d7d86299db1
AD7six <andydawson76@gmail.com>
2012-06-19 12:57:22 +0200

    adding license info

--- Log/Engine/SyslogLog.php
+++ Log/Engine/SyslogLog.php
@@ -4,6 +4,12 @@ App::uses('BaseLog', 'Log/Engine');
 /**
  * SyslogLog
  *
+ * Copyright 2010-2012, Nodes ApS. (http://www.nodesagency.com/)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright Nodes ApS, 2012
  */
 class SyslogLog extends BaseLog {
 
